d4059c90bf9e34cf1d35948305eaa261e7e73779
Radek Podgorny <radek@podgorny.cz>
2012-02-03 16:26:46 +0100

    automatic port detection and version bump

--- ipv6listen.py
+++ ipv6listen.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 import sys
 import socket
 import select
 import logging
+import time
 
 # TODO: this is version for windowed applications
 def get_output(cmd):
@@ -14,7 +15,7 @@ def get_output(cmd):
 	return f.read()
 #enddef
 
-def find_ipv4_only():
+def get_listening_ports():
 	def parse(s):
 		for i in s:
 			while '  ' in i: i = i.replace('  ', ' ')
@@ -32,31 +33,59 @@ def find_ipv4_only():
 	ipv4 = [i for i in ipv4 if 'LISTENING' in i]
 	ipv4 = parse(ipv4)
 	ipv4 = map(get_port, ipv4)
-	ipv4 = list(set(ipv4))
-
+	ipv4 = sorted(list(set(ipv4)))
+	
 	ipv6 = get_output('netstat -a -n -p tcpv6').split('\n')
 	ipv6 = [i for i in ipv6 if 'LISTENING' in i]
 	ipv6 = parse(ipv6)
 	ipv6 = map(get_port, ipv6)
-	ipv6 = list(set(ipv6))
+	ipv6 = sorted(list(set(ipv6)))
 	
+	return ipv4, ipv6
+#enddef
+
+def find_only():
+	ipv4, ipv6 = get_listening_ports()
+
 	logging.debug('ipv4: %s', ipv4)
 	logging.debug('ipv6: %s', ipv6)
 
-	ret = ipv4
+	ret4 = ipv4[:]
 	for i in ipv6:
-		if i in ret: ret.remove(i)
+		if i in ret4: ret4.remove(i)
 	#endfor
+	
+	ret6 = ipv6[:]
+	for i in ipv4:
+		if i in ret6: ret6.remove(i)
+	#endfor
+	
+	logging.debug('ipv4_only: %s', ret4)
+	logging.debug('ipv6_only: %s', ret6)
 
-	return ret
+	return ret4, ret6
+#enddef
+
+def init_logging():
+	logger = logging.getLogger()
+	logger.setLevel(logging.DEBUG)
+
+	sh = logging.StreamHandler()
+	fh = logging.FileHandler('ipv6listen.log')
+
+	sh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+	fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+	logger.addHandler(sh)
+	logger.addHandler(fh)
 #enddef
 
 def main():
-	logging.basicConfig(level=logging.DEBUG)
+	init_logging()
 
 	logging.info('*' * 40)
 	logging.info('starting ipv6listen v%s', __version__)
-	
+
 	if sys.platform == 'win32':
 		logging.info('detected win32')
 
@@ -64,30 +93,66 @@ def main():
 		tray.run('ipv6listen.png', 'ipv6listen v%s' % __version__)
 	#endif
 
-	ports = map(int, sys.argv[1:])
-	if not ports:
-		logging.info('no ports specified, using autodetection')
-		ports = find_ipv4_only()
-		logging.info('found ports: %s', ports)
+	#ports = map(int, sys.argv[1:])
+	#if not ports:
+	#	logging.info('no ports specified, using autodetection')
+	#	ports,_ = find_only()
+	#	logging.info('found ports: %s', ports)
 	#endif
 
 	sock_pairs = []
 
 	listen_socks = []
 	listen_sock_to_port_map = {}
-	for p in ports:
-		s = socket.socket(socket.AF_INET6)
-		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-		s.bind(('::', p))
-		s.listen(10)
-		logging.info('listening on port %s', p)
-		listen_socks.append(s)
-		listen_sock_to_port_map[s] = p
+	#for p in ports:
+	#	s = socket.socket(socket.AF_INET6)
+	#	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	#	s.bind(('::', p))
+	#	s.listen(10)
+	#	logging.info('listening on port %s', p)
+	#	listen_socks.append(s)
+	#	listen_sock_to_port_map[s] = p
 	#endfor
+	
+	t_last_check = 0
 
 	try:
 		run = True
 		while run and not tray._exit:
+			t = time.time()
+			
+			if t-t_last_check > 60:
+				logging.info('scanning for listening port changes')
+				
+				ipv4_only, ipv6_only = find_only()
+				
+				for p in ipv4_only:
+					if p in listen_sock_to_port_map.values(): continue
+					
+					logging.info('found new ipv4-only port %s', p)
+
+					s = socket.socket(socket.AF_INET6)
+					s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+					s.bind(('::', p))
+					s.listen(10)
+					logging.info('listening on port %s', p)
+					listen_socks.append(s)
+					listen_sock_to_port_map[s] = p
+				#endfor
+
+				for s,p in listen_sock_to_port_map.items():
+					if not p in ipv6_only: continue
+
+					logging.info('detected stale ipv6-only port %s', p)
+
+					listen_socks.remove(s)
+					del listen_sock_to_port_map[s]
+					s.close()
+				#endfor
+				
+				t_last_check = t
+			#endif
+			
 			rlist = listen_socks[:]
 			wlist = []
 			xlist = listen_socks[:]
@@ -104,7 +169,7 @@ def main():
 			for s in listen_socks:
 				if s in rlist:
 					conn, addr = s.accept()
-					logging.info('accept from %s', addr)
+					logging.info('accept from %s for socket on port %s', addr, listen_sock_to_port_map[s])
 					s2 = socket.socket(socket.AF_INET)
 					s2.connect(('127.0.0.1', listen_sock_to_port_map[s]))
 					sock_pairs.append((conn, s2))
