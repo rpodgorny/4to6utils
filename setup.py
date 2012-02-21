4a0c7770fea5ba20e281941804f6f581740ceda4
Radek Podgorny <radek@podgorny.cz>
2012-02-21 14:28:29 +0100

    first changes for the new rpc way of app/tray division

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from distutils.core import setup
 import py2exe
 
-from ipv6listen import __version__ as version
+from ipv6listen import __version__
 
 setup(
-	windows = ['ipv6listen.py', ],
-	version = version,
+	console = ['ipv6listen.py', ],
+	version = __version__,
+	zipfile = None,
+	options = {'py2exe': {'bundle_files': 1}}
+)
+
+setup(
+	windows = ['ipv6listen_tray.py', ],
+	version = __version__,
 	zipfile = None,
 	options = {'py2exe': {'bundle_files': 1}}
 )
